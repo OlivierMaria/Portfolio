5873b4bcaa2e4d931b1cce85d29ddbb5f5fdc80d
OlivierMaria <olivier-maria@hotmail.com>
2023-10-25 01:30:52 +0200

    Creation of the contact form in its respective 'Contact' component. Adding style to the form as well as the button and implementing responsive.

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,6 +1,43 @@
-import React from "react";
 import "./contact.css";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact__wrapper">
+      <h1 className="title">
+        <span className="icon-envelope"></span>
+        Me contacter
+      </h1>
+      <p className="subtitle">
+        Prêt à donner vie à vos projets ? Contactez-moi pour en savoir plus sur
+        la manière dont nous pouvons travailler ensemble pour atteindre vos
+        objectifs en ligne.
+      </p>
+
+      <div className="flex">
+        <form>
+          <div className="flex">
+            <label htmlFor="email">Adresse e-mail:</label>
+            <input
+              required
+              type="email"
+              name=""
+              id="email"
+              placeholder="Adresse e-mail"
+            />
+          </div>
+          <div className="flex" style={{ marginTop: "24px" }}>
+            <label htmlFor="message">Votre message:</label>
+            <textarea
+              required
+              name=""
+              id="message"
+              placeholder="message"
+            ></textarea>
+          </div>
+          <button className="submit">Envoyer</button>
+        </form>
+        <div className="border animation">Animation</div>
+      </div>
+    </section>
+  );
 }
