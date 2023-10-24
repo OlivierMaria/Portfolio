d8ae42f1e1e0da4da5f9be3746cc6855bcb8aff4
OlivierMaria <olivier-maria@hotmail.com>
2023-10-25 00:20:39 +0200

    Creation of the 'main' section in its respective 'Main' component, I added the style, the buttons, the cards and made it responsive.

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,5 +1,36 @@
-import React from "react";
 import "./main.css";
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <main className="flex">
+      <section className="main__left flex">
+        <button className="active">Tout les projets</button>
+        <button>HTML & CSS</button>
+        <button>JavaScript</button>
+        <button>React & MUI</button>
+        <button>NextJs</button>
+        <button>Node & Express</button>
+      </section>
+      <section className="main__right flex">
+        {["aa", "aaa", "cc", 1, 7].map((item) => {
+          return (
+            <article key={item} className="card">
+              <img width={266} src="/01.jpg" alt="" />
+
+              <div style={{ width: "266px" }} className="box__wrapper">
+                <h1 className="title">Landing Page</h1>
+                <p className="subtitle">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corporis sit{" "}
+                </p>
+                <div className="flex icons">
+                  <div className="icon-link"></div>
+                  <div className="icon-github"></div>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
