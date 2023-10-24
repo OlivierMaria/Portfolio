4d4f50308fcd8f70fb50de9ef051eb5c45a473cf
OlivierMaria <olivier-maria@hotmail.com>
2023-10-24 18:05:32 +0200

    Work done on the navigation bar on the style, addition of two buttons: theme light/dark and a modal menu button for responsive.

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,5 +1,73 @@
-import React from "react";
+import { useState } from "react";
 import "./header.css";
 export default function Header() {
-  return <header>Header</header>;
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <header className="flex">
+      <button
+        className="hamburger__menu"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Burger_Menu
+      </button>
+      <div />
+
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">A propos</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projets</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <button>light</button>
+
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal__wrapper">
+            <li>
+              <button
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                False
+              </button>
+            </li>
+            <li>
+              <a href="">A propos</a>
+            </li>
+
+            <li>
+              <a href="">Articles</a>
+            </li>
+            <li>
+              <a href="">Projets</a>
+            </li>
+            <li>
+              <a href="">Speaking</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
 }
