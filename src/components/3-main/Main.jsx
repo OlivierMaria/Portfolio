6a033a617103fba446820e6731b3ebcc20f2ca9b
OlivierMaria <olivier-maria@hotmail.com>
2023-10-25 12:18:41 +0200

    Implementation of sorting logic for projects in 'main'

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,23 +1,102 @@
+import { useState } from "react";
 import "./main.css";
+
+const myProjects = [
+  {
+    projectTitle: "CSS project",
+    category: ["css", "html"],
+    imgPath: "./01.jpg",
+  },
+  {
+    projectTitle: "JavaScript project",
+    category: ["javascript"],
+    imgPath: "./02.jpg",
+  },
+  { projectTitle: "React project", category: ["react"], imgPath: "./03.jpg" },
+  { projectTitle: "NextJs project", category: ["nextJs"], imgPath: "./04.jpg" },
+  {
+    projectTitle: "NodeJs project",
+    category: ["node", "express"],
+    imgPath: "./05.jpg",
+  },
+];
+
 export default function Main() {
+  const [currentActive, setCurrentActive] = useState("all");
+  const [arr, setArr] = useState(myProjects);
+  const handleClick = (buttonCategory) => {
+    setCurrentActive(buttonCategory);
+    const newArr = myProjects.filter((item) => {
+      const ZZZ = item.category.find((myItem) => {
+        return myItem === buttonCategory;
+      });
+      return ZZZ === buttonCategory;
+    });
+    setArr(newArr);
+  };
   return (
     <main className="flex">
       <section className="main__left flex">
-        <button className="active">Tout les projets</button>
-        <button>HTML & CSS</button>
-        <button>JavaScript</button>
-        <button>React & MUI</button>
-        <button>NextJs</button>
-        <button>Node & Express</button>
+        <button
+          onClick={() => {
+            setCurrentActive("all");
+            setArr(myProjects);
+          }}
+          className={currentActive === "all" ? "active" : null}
+        >
+          Tout les projets
+        </button>
+        <button
+          onClick={() => {
+            handleClick("css");
+          }}
+          className={currentActive === "css" ? "active" : null}
+        >
+          HTML & CSS
+        </button>
+
+        <button
+          onClick={() => {
+            handleClick("javascript");
+          }}
+          className={currentActive === "javascript" ? "active" : null}
+        >
+          JavaScript
+        </button>
+
+        <button
+          onClick={() => {
+            handleClick("react");
+          }}
+          className={currentActive === "react" ? "active" : null}
+        >
+          React & MUI
+        </button>
+        <button
+          onClick={() => {
+            handleClick("nextJs");
+          }}
+          className={currentActive === "nextJs" ? "active" : null}
+        >
+          NextJs
+        </button>
+        <button
+          onClick={() => {
+            handleClick("node");
+          }}
+          className={currentActive === "node" ? "active" : null}
+        >
+          Node & Express
+        </button>
       </section>
       <section className="main__right flex">
-        {["aa", "aaa", "cc", 1, 7].map((item) => {
+        {arr.map((item) => {
           return (
-            <article key={item} className="card">
-              <img width={266} src="/01.jpg" alt="" />
+            <article key={item.imgPath} className="card">
+              <img width={266} src={item.imgPath} alt="" />
 
               <div style={{ width: "266px" }} className="box__wrapper">
-                <h1 className="title">Landing Page</h1>
+                <h1 className="title">{item.projectTitle}</h1>
                 <p className="subtitle">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Corporis sit{" "}
